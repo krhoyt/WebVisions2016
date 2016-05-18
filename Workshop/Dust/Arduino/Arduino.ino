d781e34390a5ceb25f10c46559a8ebbde42978d2
Kevin Hoyt <parkerkrhoyt@gmail.com>
2016-05-18 06:48:44 -0700

    Added comments.
    Cleaned up variable names, and added some comments.

--- Workshop/Dust/Arduino/Arduino.ino
+++ Workshop/Dust/Arduino/Arduino.ino
@@ -1,29 +1,46 @@
+// Dust sensor
 const int DUST_PIN = 7;
 
 unsigned long duration;
-unsigned long starttime;
-unsigned long sampletime = 2000;
-unsigned long lowpulseoccupancy = 0;
+unsigned long start_time;
+unsigned long sample_time = 2000;
+unsigned long lpo = 0;
 
 float ratio = 0;
 float concentration = 0;
  
 void setup() {
+  // Serial
   Serial.begin( 9600 );
+
+  // Digital input
   pinMode( DUST_PIN, INPUT );
-  starttime = millis();
+
+  // Non-blocking timer
+  // Replacement for delay
+  start_time = millis();
 }
  
 void loop() {
+  // Read pin value
   duration = pulseIn( DUST_PIN, LOW );
-  lowpulseoccupancy = lowpulseoccupancy + duration;
- 
-  if( ( millis() - starttime ) >= sampletime ) {
-    ratio = lowpulseoccupancy / ( sampletime * 10.0 );
+  
+  lpo = lpo + duration;
+
+  // If timer has elapsed
+  // Calculate and display
+  if( ( millis() - start_time ) >= sample_time ) {
+    // Calculate
+    // http://www.seeedstudio.com/wiki/Grove_-_Dust_Sensor
+    ratio = lpo / ( sample_time * 10.0 );
     concentration = 1.1 * pow( ratio, 3 ) - 3.8 * pow( ratio, 2 ) + 520 * ratio + 0.62;
+
+    // Display
     Serial.println( concentration );
-    lowpulseoccupancy = 0;
-    starttime = millis();
+
+    // Reset clock and sample
+    lpo = 0;
+    start_time = millis();
   }
 }
 
